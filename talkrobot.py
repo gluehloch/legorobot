daa9b2f2799d249a05ff72c93b0202c20a640741
Andre Winkler <gluehloch@googlemail.com>
2018-02-17 11:06:29 +0100

    Under construction... What else...?

--- talkrobot.py
+++ talkrobot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # from ev3dev.ev3 import ev3dev.ev3 as ev3
- import ev3dev.ev3 as ev3
+import ev3dev.ev3 as ev3
 
 # TODO: Add code here
 ev3.Sound.speak('Welcome to the E V 3 dev project!').wait()
